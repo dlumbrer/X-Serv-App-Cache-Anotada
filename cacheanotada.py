6b9284ab282d9c55b63aff2012b0bbca0bd92d85
David Moreno Lumbreras <dmorenolumb@gmail.com>
2015-03-03 19:27:09 +0100

    Añadida cache con su respectivo boton

--- cacheanotada.py
+++ cacheanotada.py
@@ -14,25 +14,37 @@ class escrituraApp(webapp.webApp):
 
     def process(self, rec):
         urloriginal = "http://" + rec
-        try:
-            f = urllib.urlopen(urloriginal)
-            html = f.read()
-            
-            #Guardar en cache
-            cache[rec] = html
-            
-            pos1 = html.find("<body")
-            #posicionAbsoluta = html.find(">", pos1) OTRA FORMA
-            pos2 = html[pos1:].find(">")
-            posicionAbsoluta = pos1+pos2+1
-            
-            html1 = html[:posicionAbsoluta]
-            html2 = html[posicionAbsoluta:]
-            
-            html = html1 + "<p>HOLA " + "<a href='" + urloriginal + "'>Original</a>" + "\r" + "<a href='http://localhost:1234/" + rec + "'>Recarga</a>" + "\r" + "<a href='http://localhost:1234/cache/" + rec + "'>Cache</a>" + "</p>" + html2
-            return ("200 OK", html)
-        except IOError:
-            return ("404 Not Found", "Error: Imposible conectar")
+	
+	partes = rec.split("/")
+	print "--------------------------------------------------" + str(partes)
+	
+	if partes[0] != "cache":
+	    try:
+		f = urllib.urlopen(urloriginal)
+		html = f.read()
+		
+		#Guardar en cache
+		cache[rec] = html
+		
+		pos1 = html.find("<body")
+		#posicionAbsoluta = html.find(">", pos1) OTRA FORMA
+		pos2 = html[pos1:].find(">")
+		posicionAbsoluta = pos1+pos2+1
+		
+		html1 = html[:posicionAbsoluta]
+		html2 = html[posicionAbsoluta:]
+		
+		html = html1 + "<p>HOLA " + "<a href='" + urloriginal + "'>Original</a>" + "\r" + "<a href='http://localhost:1234/" + rec + "'>Recarga</a>" + "\r" + "<a href='http://localhost:1234/cache/" + rec + "'>Cache</a>" + "</p>" + html2
+		return ("200 OK", html)
+	    except IOError:
+		return ("404 Not Found", "Error: Imposible conectar")
+		
+	elif partes[0] == "cache":
+	    try:
+		html = cache[partes[1]]
+		return ("200 OK", html)
+	    except KeyError:
+		return ("404 Not Found", "Error: No hay cache almacenada para este sitio")
             
 
 if __name__ == "__main__":
